d9cbead88ef26f7b30b1a7b9c3e9c56a9c2e8cdc
Hishamahmed97 <Hisham.ahmed97@outlook.com>
2024-05-24 16:55:00 +0300

    template.cpp

--- OOP/template.cpp
+++ OOP/template.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
+// template for each function just before them --> generic programming
+
 template<typename T>
 T sum(T x,T y)
 {
     return x+y;
 }
 
-
+template<typename N>
+N sub(N x,N y)
+{
+    return x-y;
+}
 int main()
 {
-float x =5;float y = 6.5;
+float x = 5;float y = 6.5;
 std::cout<<sum(x,y)<<std::endl;
+std::cout<<sub(x,y)<<std::endl;
 return 0;
 }
