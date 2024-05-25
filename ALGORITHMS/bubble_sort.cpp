e7776427c5a5a04609324c3fb87fe26c354117ce
Hishamahmed97 <Hisham.ahmed97@outlook.com>
2024-05-25 16:39:41 +0300

    bbsrt

--- ALGORITHMS/bubble_sort.cpp
+++ ALGORITHMS/bubble_sort.cpp
@@ -8,9 +8,9 @@ void swap(int * one,int * two)
     *two = temp;
 }
 
-void 
+void bubble_sort(int arr[],int size)
 {
-    for (int = 0;i<size-1;i++)
+    for (int i= 0;i<size-1;i++)
     {
         for (int j = 0;j < size-i-1;j++)
         {
